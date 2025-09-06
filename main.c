11581fa41ec81c6e71f5b55c0c530a056ced4784
daniela <a110636@alunos.uminho.pt>
2025-09-06 12:40:31 +0100

    11 para sair, char de entrada para controlo melhor

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "def.h"
 #include "funcoes.c"
 #include <ncurses.h>
+#include <string.h>
 
 int main(){
     char tabuleiro[linhas][colunas];
@@ -13,29 +14,34 @@ int main(){
 
     criarVisivel(visivel);
     imprimirVisivel(visivel);
-    
-    char c1,c2,cB1,cB2;
-    while(1){
-        printf("\nRevelar coordenada ('00' para BANDEIRA): ");
-        scanf(" %c %c",&c1,&c2);
+        
+    char entrada[3], cB1,cB2;
+    int semParar = 1;
 
-        if(c1=='0' && c2=='0'){
+    while (semParar) {
+        printf("\nRevelar coordenada ('00' para BANDEIRA, '11' para SAIR): ");
+        scanf("%2s", entrada);
+
+        if (strcmp(entrada,"00")==0) {
             printf("Coordenadas da bandeira: ");
-            scanf(" %c %c", &cB1,&cB2);
-            int nB1 = cB1-'a';
-            int nB2 = cB2-'a';
-            porBandeira(nB1,nB2,tabuleiro,visivel);
-            imprimirVisivel(visivel);
-        } else{
-            int x = c1-'a';
-            int y = c2-'a';
-            revelar(x,y,tabuleiro,visivel);
+            scanf(" %c %c", &cB1, &cB2);
+            int nB1 = cB1 - 'a';
+            int nB2 = cB2 - 'a';
+            porBandeira(nB1, nB2, tabuleiro, visivel);
             imprimirVisivel(visivel);
 
-            if(tabuleiro[x][y]=='!'){
+        } else if (strcmp(entrada,"11")==0) {
+            semParar = 0;  
+        } else {
+            int x = entrada[0] - 'a';
+            int y = entrada[1] - 'a';
+            revelar(x, y, tabuleiro, visivel);
+            imprimirVisivel(visivel);
+
+            if (tabuleiro[x][y] == '!') {
                 imprimirTab(tabuleiro);
                 printf("BOOM!\n");
-                break;
+                semParar = 0;  
             }
         }
     }
